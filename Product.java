29f9d5aaab849e2ff76cfcb0deb88025e5cd44a2
Aparna-Jayakumar <125530532+Aparna-Jayakumar@users.noreply.github.com>
2023-03-24 15:49:30 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,33 @@
+public class Product
+{
+  String pname,pcode;
+ int price;
+  void getdata(String p1, String p2,int p3)
+  {
+    pname=p1;
+    pcode=p2;
+    price=p3;
+   
+  }
+public static void main(String[] args)
+{
+System.out.println("Name : Aparna Jayakumar");
+System.out.println("Admission_no: 22MCA012");
+System.out.println("Date :24/03/2023");
+int s;
+Product a,b,c;
+a=new Product();
+b=new Product();
+c=new Product();
+a.getdata("1045A","apple",20);
+b.getdata("1056B","stawberry",40);
+c.getdata("1098C","orange",30);
+if(a.price<b.price  && a.price<c.price)
+ s=a.price;
+else if(b.price<a.price && b.price<c.price)
+s=b.price;
+else
+s=c.price;
+System.out.println("Smallest price=  "+s);
+}
+}
